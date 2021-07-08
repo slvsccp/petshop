de347b9778375653f1811f40f3e2bb3b61224577
Matheus silva <teus_26@hotmail.com>
2021-07-08 15:30:11 -0300

    menu and about

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert('hellooooooooooooooooo')
+/* open and close the menu, when click in icon: hamburguer and X*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* when clicking on a menu item, hide the menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
