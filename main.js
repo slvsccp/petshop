168f0046303f025e537b7274caf5cb4aec45f948
Matheus silva <teus_26@hotmail.com>
2021-07-09 15:39:05 -0300

    smooth scroll/ header scroll/ services

--- main.js
+++ main.js
@@ -16,3 +16,15 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* change header when scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
